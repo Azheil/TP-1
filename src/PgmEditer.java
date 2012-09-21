a8c10cb65e6ac7f23f40f0738ab95a47b64ce1f1
Azheil <sebastien.malkowiak@eleves.ec-nantes.fr>
2012-09-21 12:06:08 +0200

    commit #2

--- src/PgmEditer.java
+++ src/PgmEditer.java
@@ -1,3 +1,6 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * 21 sept. 2012
  * Seb
@@ -14,8 +17,30 @@ public class PgmEditer {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Stub de la méthode généré automatiquement
+		System.out.println("PGM Editer");
+		System.out.println("----------------------");
+		System.out.println("\nEntrer le nom de fichier");
+		Scanner sc = new Scanner(System.in);
+		String filePath = sc.next();
+		
+		try {
+			ShortPixmap image = new ShortPixmap(filePath);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
+
+	}
+	
+	public void histogramme(ShortPixmap img) {
+		short[] histogramme = new short[256];
+		
+		for (int i=0; i<=img.width; i++) {
+			for (int j=0; j<=img.height; i++ ) {
+				//int k = img.
+				//histogramme[k]++; 
+			}
+		}
 	}
 
 }
